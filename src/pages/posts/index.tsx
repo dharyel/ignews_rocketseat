82febd4db01de0c55bfecd0701a8e42a26034aff
dharyel <dshfoxdev@gmail.com>
2021-10-25 07:50:12 -0300

    página post utilizando slug criada, no arquivo [slug].tsx

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.scss';
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+
 
 type Post = {
     slug: string;
@@ -26,11 +28,13 @@ export default function Posts({posts}: PostProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <a key={post.slug} href="#">
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
+                        <Link href={`/posts/${post.slug}`} key={post.slug}>
+                            <a >
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                     ))}
                 </div>
             </main>
